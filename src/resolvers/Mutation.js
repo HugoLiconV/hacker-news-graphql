3f7a09b79e1c64242f930d043a51db0b7eeb4b4f
Hugo <hugo_val28@hotmail.com>
2019-07-06 16:48:11 -0600

    add subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -49,8 +49,29 @@ async function login(parent, args, context, info) {
   };
 }
 
+async function vote(parent, args, context, info) {
+  // 1
+  const userId = getUserId(context)
+
+  // 2
+  const linkExists = await context.prisma.$exists.vote({
+    user: { id: userId },
+    link: { id: args.linkId },
+  })
+  if (linkExists) {
+    throw new Error(`Already voted for link: ${args.linkId}`)
+  }
+
+  // 3
+  return context.prisma.createVote({
+    user: { connect: { id: userId } },
+    link: { connect: { id: args.linkId } },
+  })
+}
+
 module.exports = {
   signup,
   login,
-  post
+  post,
+  vote
 };
